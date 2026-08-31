104ea722c0f0707b8043c370473856015b0740af
polskiftw <polskiftw@gmail.com>
2026-08-31 11:08:38 -0400

    Fix shopping planner escape regression

--- terraria-shopping/app.js
+++ terraria-shopping/app.js
@@ -59,7 +59,7 @@ if(fishMode){try{fishMode.checked=localStorage.getItem('terraria-shopping-fish-m
 let fishModeProgress=fishMode?.checked?{...acquired}:null;
 const fishModeEligibility=new Map();
 const save=()=>localStorage.setItem('terraria-shopping-progress-v2',JSON.stringify(acquired));
-const esc=s=>String(s??'').replace(/[&<>"']/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot',"'":'&#039;'}[c]));
+const esc=s=>String(s??'').replace(/[&<>"']/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#039;'}[c]));
 const wikiName=name=>W+encodeURIComponent(String(name).replaceAll(' ','_'));
 const wikiItem=id=>W+(items[id]?.wiki||encodeURIComponent(String(items[id]?.name||id).replaceAll(' ','_')));
 const avObject=tuple=>{
