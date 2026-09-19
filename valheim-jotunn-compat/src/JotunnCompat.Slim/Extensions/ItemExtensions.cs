c79a963bfee4c79b442c3babfd11dce8d4e73ddd
polskiftw <polskiftw@gmail.com>
2026-09-19 04:48:07 -0400

    Vendor slim Jotunn source: Extensions/ItemExtensions.cs

---
+++ valheim-jotunn-compat/src/JotunnCompat.Slim/Extensions/ItemExtensions.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using UnityEngine;
+
+namespace Jotunn
+{
+    /// <summary>
+    ///     Extends GameObject with a check if the GameObject is valid
+    /// </summary>
+    public static class GameObjectExtension
+    {
+        /// <summary>
+        ///     Check for validity
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static bool IsValid(this GameObject self)
+        {
+            var name = self.name;
+            if (name.IndexOf('(') > 0)
+            {
+                name = name.Substring(self.name.IndexOf('(')).Trim();
+            }
+
+            if (string.IsNullOrEmpty(name))
+            {
+                Logger.LogError($"GameObject must have a name!");
+                return false;
+            }
+
+            if (self.GetComponent<ZNetView>() && self.name.IndexOfAny(new[] { ')', ' ' }) > 0)
+            {
+                Logger.LogError($"GameObject name '{self.name}' must not contain parenthesis or spaces!");
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    /// <summary>
+    ///     Extends ItemDrop with a TokenName
+    /// </summary>
+    public static class ItemDropExtension
+    {
+        /// <summary>
+        ///     m_itemData.m_shared.m_name
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static string TokenName(this ItemDrop self) => self.m_itemData.m_shared.m_name;
+    }
+
+    /// <summary>
+    ///     Extends ItemData with a TokenName.
+    /// </summary>
+    public static class ItemDataExtension
+    {
+        /// <summary>
+        ///     m_shared.m_name
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static string TokenName(this ItemDrop.ItemData self) => self.m_shared.m_name;
+    }
+
+    /// <summary>
+    ///     Extends StatusEffect with a TokenName and a check if the StatusEffect is valid so it can be added to the game.
+    /// </summary>
+    public static class RecipeExtension
+    {
+        /// <summary>
+        ///     Check for validity
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static bool IsValid(this Recipe self)
+        {
+            try
+            {
+                var name = self.name;
+                if (name.IndexOf('(') > 0)
+                {
+                    name = name.Substring(self.name.IndexOf('(')).Trim();
+                }
+                if (string.IsNullOrEmpty(name))
+                {
+                    throw new Exception($"Recipe must have a name !");
+                }
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e);
+
+                return false;
+            }
+        }
+    }
+
+    /// <summary>
+    ///     Extends Piece with a TokenName
+    /// </summary>
+    public static class PieceExtension
+    {
+        /// <summary>
+        ///     m_name
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static string TokenName(this Piece self) => self.m_name;
+    }
+
+    /// <summary>
+    ///     Extends StatusEffect with a TokenName and a check if the StatusEffect is valid so it can be added to the game.
+    /// </summary>
+    public static class StatusEffectExtension
+    {
+        /// <summary>
+        ///     m_name
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static string TokenName(this StatusEffect self) => self.m_name;
+
+        /// <summary>
+        ///     Check for validity
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static bool IsValid(this StatusEffect self)
+        {
+            try
+            {
+                var name = self.name;
+                if (name.IndexOf('(') > 0)
+                {
+                    name = name.Substring(self.name.IndexOf('(')).Trim();
+                }
+                if (string.IsNullOrEmpty(name))
+                {
+                    throw new Exception($"StatusEffect must have a name !");
+                }
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e);
+
+                return false;
+            }
+        }
+    }
+}
