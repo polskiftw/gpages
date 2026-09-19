b5a32cf5e565ec51029fd36596784c19edbf5d5d
polskiftw <polskiftw@gmail.com>
2026-09-19 04:44:44 -0400

    Vendor slim Jotunn source: Entities/CustomPrefab.cs

---
+++ valheim-jotunn-compat/src/JotunnCompat.Slim/Entities/CustomPrefab.cs
@@ -0,0 +1,107 @@
+﻿using System.Reflection;
+using BepInEx;
+using Jotunn.Managers;
+using Jotunn.Utils;
+using UnityEngine;
+
+namespace Jotunn.Entities
+{
+    /// <summary>
+    ///     Wrapper for custom added GameObjects holding the mod reference.
+    /// </summary>
+    public class CustomPrefab : CustomEntity, IModPrefab
+    {
+        /// <summary>
+        ///     Original prefab
+        /// </summary>
+        public GameObject Prefab { get; }
+
+        /// <summary>
+        ///     Indicator if references from <see cref="Entities.Mock{T}"/>s will be replaced at runtime.
+        /// </summary>
+        public bool FixReference { get; set; }
+
+        private string PrefabName
+        {
+            get => Prefab ? Prefab.name : fallbackPrefabName;
+        }
+
+        private string fallbackPrefabName;
+
+        /// <summary>
+        ///     Internal ctor with provided <see cref="BepInPlugin"/> metadata.<br />
+        ///     Does not fix references.
+        /// </summary>
+        /// <param name="prefab">Prefab added</param>
+        /// <param name="sourceMod">Metadata of the mod adding this prefab</param>
+        internal CustomPrefab(GameObject prefab, BepInPlugin sourceMod) : base(sourceMod)
+        {
+            Prefab = prefab;
+        }
+
+        /// <summary>
+        ///     Custom prefab.<br />
+        ///     Can fix references for <see cref="Entities.Mock{T}"/>s.
+        /// </summary>
+        /// <param name="prefab">The prefab for this custom item.</param>
+        /// <param name="fixReference">If true references for <see cref="Entities.Mock{T}"/> objects get resolved at runtime by Jötunn.</param>
+        public CustomPrefab(GameObject prefab, bool fixReference) : base(Assembly.GetCallingAssembly())
+        {
+            Prefab = prefab;
+            FixReference = fixReference;
+        }
+
+        /// <summary>
+        ///     Custom prefab loaded from an <see cref="AssetBundle"/>.<br />
+        ///     Can fix references for <see cref="Entities.Mock{T}"/>s.
+        /// </summary>
+        /// <param name="assetBundle">A preloaded <see cref="AssetBundle"/></param>
+        /// <param name="assetName">Name of the prefab in the bundle.</param>
+        /// <param name="fixReference">If true references for <see cref="Entities.Mock{T}"/> objects get resolved at runtime by Jötunn.</param>
+        public CustomPrefab(AssetBundle assetBundle, string assetName, bool fixReference) : base(Assembly.GetCallingAssembly())
+        {
+            fallbackPrefabName = assetName;
+
+            if (!AssetUtils.TryLoadPrefab(SourceMod, assetBundle, assetName, out GameObject prefab))
+            {
+                return;
+            }
+
+            Prefab = prefab;
+            FixReference = fixReference;
+        }
+
+        /// <summary>
+        ///     Checks if a custom item is valid (i.e. has a prefab, an <see cref="ItemDrop"/> and an icon, if it should be craftable).
+        /// </summary>
+        /// <returns>true if all criteria is met</returns>
+        public bool IsValid()
+        {
+            bool valid = true;
+
+            if (!Prefab)
+            {
+                Logger.LogError(SourceMod, $"CustomPrefab '{this}' has no prefab");
+                valid = false;
+            }
+
+            return valid;
+        }
+
+        /// <summary>
+        ///     Helper method to determine if a prefab with a given name is a custom prefab created with Jötunn.
+        /// </summary>
+        /// <param name="prefabName">Name of the prefab to test.</param>
+        /// <returns>true if the prefab is added as a custom prefab to the <see cref="PrefabManager"/>.</returns>
+        public static bool IsCustomPrefab(string prefabName)
+        {
+            return PrefabManager.Instance.Prefabs.ContainsKey(prefabName);
+        }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return PrefabName;
+        }
+    }
+}
