918a753022a6f5d9c17b78721e83e42c3f0ab6d8
polskiftw <polskiftw@gmail.com>
2026-09-19 04:12:52 -0400

    Validate Jotunn preloader discovery contract

--- valheim-jotunn-compat/src/JotunnCompat.ContractCheck/Program.cs
+++ valheim-jotunn-compat/src/JotunnCompat.ContractCheck/Program.cs
@@ -1,62 +1,137 @@
 using Mono.Cecil;
 
-if (args.Length != 1)
+if (args.Length is < 1 or > 2)
 {
-    Console.Error.WriteLine("Usage: JotunnCompat.ContractCheck <Jotunn.dll>");
+    Console.Error.WriteLine(
+        "Usage: JotunnCompat.ContractCheck <Jotunn.dll> [JotunnCompat.Preloader.dll]");
     return 2;
 }
 
-var path = Path.GetFullPath(args[0]);
-if (!File.Exists(path))
+var jotunnPath = Path.GetFullPath(args[0]);
+if (!File.Exists(jotunnPath))
 {
-    throw new FileNotFoundException("Jotunn.dll not found", path);
+    throw new FileNotFoundException("Jotunn.dll not found", jotunnPath);
 }
 
-using var module = ModuleDefinition.ReadModule(path, new ReaderParameters
+using (var module = ModuleDefinition.ReadModule(jotunnPath, new ReaderParameters
 {
     ReadingMode = ReadingMode.Deferred,
     ReadSymbols = false
-});
-
-if (!string.Equals(module.Assembly.Name.Name, "Jotunn", StringComparison.Ordinal))
+}))
 {
-    throw new InvalidDataException(
-        $"Expected assembly name 'Jotunn', got '{module.Assembly.Name.Name}'.");
+    if (!string.Equals(module.Assembly.Name.Name, "Jotunn", StringComparison.Ordinal))
+    {
+        throw new InvalidDataException(
+            $"Expected assembly name 'Jotunn', got '{module.Assembly.Name.Name}'.");
+    }
+
+    var expectedVersion = new Version(2, 30, 1, 0);
+    if (module.Assembly.Name.Version != expectedVersion)
+    {
+        throw new InvalidDataException(
+            $"Expected Jotunn assembly version {expectedVersion}, got {module.Assembly.Name.Version}.");
+    }
+
+    RequireMethod(module, "Jotunn.Utils.PatchInit", "InitializePatches");
+    RequireMethod(module, "Jotunn.Utils.AutomaticLocalizationsLoading", "Init");
+    RequireMethod(
+        module,
+        "Jotunn.Managers.MockManager",
+        "FixReferences",
+        "System.Object",
+        "System.Int32");
+    RequireMethod(
+        module,
+        "Jotunn.Managers.MockManager",
+        "GetRealPrefabFromMock",
+        "UnityEngine.Object",
+        "System.Type");
+    RequireMethod(
+        module,
+        "Jotunn.Managers.PrefabManager/Cache",
+        "GetPrefab",
+        "System.Type",
+        "System.String");
+    RequireMethod(
+        module,
+        "Jotunn.Managers.PrefabManager/Cache",
+        "Clear");
+    RequireMethod(
+        module,
+        "Jotunn.Managers.AssetManager/Patches",
+        "AssetBundleLoader_GetAllAssetPathsMappedToAssetID",
+        "System.Collections.Generic.IEnumerable`1<HarmonyLib.CodeInstruction>");
+
+    Console.WriteLine($"Jotunn contract check passed: {module.Assembly.Name.FullName}");
 }
 
-var expectedVersion = new Version(2, 30, 1, 0);
-if (module.Assembly.Name.Version != expectedVersion)
+if (args.Length == 2)
 {
-    throw new InvalidDataException(
-        $"Expected Jotunn assembly version {expectedVersion}, got {module.Assembly.Name.Version}.");
+    var preloaderPath = Path.GetFullPath(args[1]);
+    if (!File.Exists(preloaderPath))
+    {
+        throw new FileNotFoundException(
+            "JotunnCompat.Preloader.dll not found",
+            preloaderPath);
+    }
+
+    using var preloader = ModuleDefinition.ReadModule(preloaderPath, new ReaderParameters
+    {
+        ReadingMode = ReadingMode.Deferred,
+        ReadSymbols = false
+    });
+
+    if (!string.Equals(
+            preloader.Assembly.Name.Name,
+            "JotunnCompat.Preloader",
+            StringComparison.Ordinal))
+    {
+        throw new InvalidDataException(
+            $"Expected preloader assembly name 'JotunnCompat.Preloader', got " +
+            $"'{preloader.Assembly.Name.Name}'.");
+    }
+
+    var patcher = FindType(preloader, "JotunnCompat.Preloader.Patcher")
+        ?? throw new MissingMemberException(
+            "Required preloader type not found: JotunnCompat.Preloader.Patcher");
+
+    if (!(patcher.IsPublic && patcher.IsAbstract && patcher.IsSealed))
+    {
+        throw new InvalidDataException(
+            "JotunnCompat.Preloader.Patcher must be a public static class.");
+    }
+
+    var initialize = RequireMethod(
+        preloader,
+        "JotunnCompat.Preloader.Patcher",
+        "Initialize");
+    RequirePublicStaticVoid(initialize);
+
+    var patch = RequireMethod(
+        preloader,
+        "JotunnCompat.Preloader.Patcher",
+        "Patch",
+        "Mono.Cecil.AssemblyDefinition&");
+    RequirePublicStaticVoid(patch);
+
+    var targetDlls = RequireMethod(
+        preloader,
+        "JotunnCompat.Preloader.Patcher",
+        "get_TargetDLLs");
+    if (!(targetDlls.IsPublic && targetDlls.IsStatic) ||
+        !string.Equals(
+            targetDlls.ReturnType.FullName,
+            "System.Collections.Generic.IEnumerable`1<System.String>",
+            StringComparison.Ordinal))
+    {
+        throw new InvalidDataException(
+            "Patcher.TargetDLLs must be a public static IEnumerable<string> getter.");
+    }
+
+    Console.WriteLine(
+        $"BepInEx 5 patcher contract check passed: {preloader.Assembly.Name.FullName}");
 }
 
-RequireMethod(module, "Jotunn.Utils.PatchInit", "InitializePatches");
-RequireMethod(module, "Jotunn.Utils.AutomaticLocalizationsLoading", "Init");
-RequireMethod(
-    module,
-    "Jotunn.Managers.MockManager",
-    "FixReferences",
-    "System.Object",
-    "System.Int32");
-RequireMethod(
-    module,
-    "Jotunn.Managers.MockManager",
-    "GetRealPrefabFromMock",
-    "UnityEngine.Object",
-    "System.Type");
-RequireMethod(
-    module,
-    "Jotunn.Managers.PrefabManager/Cache",
-    "GetPrefab",
-    "System.Type",
-    "System.String");
-RequireMethod(
-    module,
-    "Jotunn.Managers.PrefabManager/Cache",
-    "Clear");
-
-Console.WriteLine($"Jotunn contract check passed: {module.Assembly.Name.FullName}");
 return 0;
 
 static MethodDefinition RequireMethod(
@@ -97,6 +172,16 @@ static MethodDefinition RequireMethod(
     throw new MissingMethodException(
         typeFullName,
         methodName + "(" + string.Join(", ", parameterTypes) + ")");
+}
+
+static void RequirePublicStaticVoid(MethodDefinition method)
+{
+    if (!(method.IsPublic && method.IsStatic) ||
+        !string.Equals(method.ReturnType.FullName, "System.Void", StringComparison.Ordinal))
+    {
+        throw new InvalidDataException(
+            $"{method.FullName} must be public static void.");
+    }
 }
 
 static TypeDefinition? FindType(ModuleDefinition module, string fullName)
