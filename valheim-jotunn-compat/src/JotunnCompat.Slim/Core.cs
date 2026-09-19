30e8cb776f427b5860a104715d39ba11d3ab7680
polskiftw <polskiftw@gmail.com>
2026-09-19 07:10:04 -0400

    Expand generic Jotunn legacy API surface

--- valheim-jotunn-compat/src/JotunnCompat.Slim/Core.cs
+++ valheim-jotunn-compat/src/JotunnCompat.Slim/Core.cs
@@ -2,11 +2,12 @@ using BepInEx.Logging;
 
 namespace Jotunn
 {
-    internal static class Logger
+    public static class Logger
     {
         private static readonly ManualLogSource Log = BepInEx.Logging.Logger.CreateLogSource(Main.ModName);
         public static void LogDebug(object value) => Log.LogDebug(value);
         public static void LogInfo(object value) => Log.LogInfo(value);
+        public static void LogMessage(object value) => Log.LogMessage(value);
         public static void LogWarning(object value) => Log.LogWarning(value);
         public static void LogError(object value) => Log.LogError(value);
     }
