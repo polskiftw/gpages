bf7b327808aecc6e6172dc787cce04e48f5c410f
polskiftw <polskiftw@gmail.com>
2026-09-19 04:45:20 -0400

    Vendor slim Jotunn source: Entities/MockRequirement.cs

---
+++ valheim-jotunn-compat/src/JotunnCompat.Slim/Entities/MockRequirement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace Jotunn.Entities
+{
+    /// <summary>
+    ///     Helper class for creating Mocks of item/piece requirements.
+    /// </summary>
+    public static class MockRequirement
+    {
+        /// <summary>
+        ///     Creates a mocked Piece.Requirement
+        /// </summary>
+        /// <param name="name">Prefab name</param>
+        /// <param name="amount">Amount</param>
+        /// <param name="recover">Whether the item is dropped after deconstructing a piece</param>
+        /// <returns></returns>
+        public static Piece.Requirement Create(string name, int amount = 1, bool recover = true)
+        {
+            var requirement = new Piece.Requirement
+            {
+                m_recover = recover,
+                m_amount = amount
+            };
+
+            requirement.m_resItem = Mock<ItemDrop>.Create(name);
+
+            return requirement;
+        }
+
+        /// <summary>
+        ///     Creates a mocked Piece.Requirement array
+        /// </summary>
+        /// <param name="requirements">List of prefab names and amounts</param>
+        /// <param name="recover">Whether the items are dropped after deconstructing a piece</param>
+        /// <returns></returns>
+        public static Piece.Requirement[] CreateArray(Dictionary<string, int> requirements, bool recover = true)
+        {
+            List<Piece.Requirement> list = new List<Piece.Requirement>();
+
+            foreach (KeyValuePair<string, int> requirement in requirements)
+            {
+                Piece.Requirement piece = Create(requirement.Key, requirement.Value, recover);
+                piece.FixReferences();
+                list.Add(piece);
+            }
+
+            return list.ToArray();
+        }
+    }
+}
