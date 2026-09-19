4422be6c9e64eb77690d7ea21df792ad4fc18959
polskiftw <polskiftw@gmail.com>
2026-09-19 04:45:54 -0400

    Vendor slim Jotunn source: Configs/SmelterConversionConfig.cs

---
+++ valheim-jotunn-compat/src/JotunnCompat.Slim/Configs/SmelterConversionConfig.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using Jotunn.Entities;
+
+namespace Jotunn.Configs
+{
+    /// <summary>
+    ///     Used to add new ItemConversions to the Smelter
+    /// </summary>
+    public class SmelterConversionConfig : ConversionConfig
+    {
+        /// <summary>
+        ///     The name of the station prefab this conversion is added to. Defaults to <see cref="Smelters.Smelter"/>.
+        /// </summary>
+        public override string Station
+        {
+            get => station;
+            set => station = Smelters.GetInternalName(value);
+        }
+
+        private string station = Smelters.Smelter;
+
+        /// <summary>
+        ///     Turns the SmelterConversionConfig into a Valheim Smelter.ItemConversion item.
+        /// </summary>
+        /// <returns>The Valheim Smelter.ItemConversion</returns>
+        public Smelter.ItemConversion GetItemConversion()
+        {
+            Smelter.ItemConversion conv = new Smelter.ItemConversion()
+            {
+                m_from = Mock<ItemDrop>.Create(FromItem),
+                m_to = Mock<ItemDrop>.Create(ToItem),
+            };
+
+            return conv;
+        }
+
+        /// <summary>
+        ///     Loads a single SmelterConversionConfig from a JSON string
+        /// </summary>
+        /// <param name="json">JSON text</param>
+        /// <returns>Loaded SmelterConversionConfig</returns>
+        public static SmelterConversionConfig FromJson(string json)
+        {
+            return SimpleJson.SimpleJson.DeserializeObject<SmelterConversionConfig>(json);
+        }
+
+        /// <summary>
+        ///     Loads a list of SmelterConversionConfigs from a JSON string
+        /// </summary>
+        /// <param name="json">JSON text</param>
+        /// <returns>Loaded list of SmelterConversionConfigs</returns>
+        public static List<SmelterConversionConfig> ListFromJson(string json)
+        {
+            return SimpleJson.SimpleJson.DeserializeObject<List<SmelterConversionConfig>>(json);
+        }
+    }
+}
