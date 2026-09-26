6bf13e0b14761660321651b9b57a77eea29676ce
polskiftw <polskiftw@gmail.com>
2026-09-26 11:32:33 -0400

    Simplify Krea2 GUI installer

--- krea2-gui/install-user.sh
+++ krea2-gui/install-user.sh
@@ -3,13 +3,10 @@ set -eu
 
 src_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 bin_dir="$HOME/.local/bin"
-lib_dir="$HOME/.local/lib/krea2-gui"
 app_dir="$HOME/.local/share/applications"
 
-mkdir -p "$bin_dir" "$lib_dir" "$app_dir"
+mkdir -p "$bin_dir" "$app_dir"
 install -m 0755 "$src_dir/krea2_gui.py" "$bin_dir/krea2-gui"
-install -m 0644 "$src_dir/krea2_anypaint_editor.py" "$bin_dir/krea2_anypaint_editor.py"
-install -m 0644 "$src_dir/krea2_anypaint.py" "$lib_dir/krea2_anypaint.py"
 install -m 0755 "$src_dir/setup-anypaint.sh" "$bin_dir/krea2-anypaint-setup"
 
 cat > "$app_dir/krea2-gui.desktop" <<DESKTOP
@@ -24,7 +21,5 @@ StartupNotify=true
 DESKTOP
 
 printf 'Installed %s\n' "$bin_dir/krea2-gui"
-printf 'Installed %s\n' "$bin_dir/krea2_anypaint_editor.py"
-printf 'Installed %s\n' "$lib_dir/krea2_anypaint.py"
 printf 'Installed %s\n' "$bin_dir/krea2-anypaint-setup"
 printf 'Installed %s\n' "$app_dir/krea2-gui.desktop"
